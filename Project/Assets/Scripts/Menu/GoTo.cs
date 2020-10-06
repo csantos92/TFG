b340ec2ac3d1f0491ea3665e72b74bb82ff62e6f
Carlos Santos <50203409+csantos92@users.noreply.github.com>
2020-10-06 19:26:23 +0200

    Upload project

---
+++ Project/Assets/Scripts/Menu/GoTo.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class GoTo : MonoBehaviour
+{
+    public void CloseGame()
+    {
+        //SFXManager.SharedInstance.PlaySFX(SFXType.SoundType.MENU);
+
+        Application.Quit();
+    }
+
+    public void GoToMainScreen()
+    {
+        //SFXManager.SharedInstance.PlaySFX(SFXType.SoundType.MENU);
+
+        SceneManager.LoadScene(0);
+    }
+
+    public void GoToTutorial()
+    {
+        //SFXManager.SharedInstance.PlaySFX(SFXType.SoundType.MENU);
+
+        SceneManager.LoadScene(1);
+    }
+
+    public void StartGame()
+    {
+        //SFXManager.SharedInstance.PlaySFX(SFXType.SoundType.MENU);
+
+        SceneManager.LoadScene(2);
+    }
+
+}
