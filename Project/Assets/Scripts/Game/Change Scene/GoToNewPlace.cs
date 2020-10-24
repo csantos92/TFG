a97cbf9f467e8f0c470fdd79edc5f31e040ba0bb
Carlos Santos <50203409+csantos92@users.noreply.github.com>
2020-10-24 16:35:53 +0200

    Update

---
+++ Project/Assets/Scripts/Game/Change Scene/GoToNewPlace.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GoToNewPlace : MonoBehaviour
+{
+
+    public string newPlaceName = "New Scene name here";
+    public bool needsClick = false;
+
+    public string uuid;
+
+    private PlayerController playerController;
+
+    private void Start()
+    {
+        playerController = FindObjectOfType<PlayerController>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Teleport(collision.gameObject.name);
+
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        Teleport(collision.gameObject.name);
+    }
+
+    private void Teleport(string name)
+    {
+        if(name == "Player")
+        {
+            if (!needsClick || (needsClick && Input.GetButton("Fire1")))
+            {
+                FindObjectOfType<PlayerController>().nextUuid = uuid;
+                SceneManager.LoadScene(newPlaceName);
+            }
+        }
+        
+        
+    }
+
+}
